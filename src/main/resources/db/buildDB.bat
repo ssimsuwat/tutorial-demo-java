57778fe3ebdda9bd6481cb1aefdc9881e2637402
Spider-Tutorials <mouse0407@163.com>
2016-04-08 23:40:12 +0800

    Add column Region to table City

--- src/main/resources/db/buildDB.bat
+++ src/main/resources/db/buildDB.bat
@@ -3,6 +3,5 @@ SET DIR=%~d0%~p0%
 
 SET connstring="data source=TUTORIAL\TUTORIAL;initial catalog=Northwind;integrated security=True"
 SET repository.path="https://github.com/espiderinc/tutorial-demo-java.git"
- 
-"D:\workspace\screencast\download\RoundhousE\console\rh.exe" /c=%connstring% /r=%repository.path% /env=LOCAL /ni /drop
+
 "D:\workspace\screencast\download\RoundhousE\console\rh.exe" /c=%connstring% /r=%repository.path% /env=LOCAL /ni /simple
