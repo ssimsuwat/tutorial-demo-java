0f0cad2bbce9bc371aa9abe444ff6f2dc5d6b2bf
SpiderTutorial <spidertutorial@outlook.com>
2016-04-24 23:41:00 +0800

    Change test server port to 8282

--- src/main/resources/db/buildDB.bat
+++ src/main/resources/db/buildDB.bat
@@ -3,7 +3,7 @@ SET DIR=%~d0%~p0%
 
 SET connstring="data source=TUTORIAL\TUTORIAL;initial catalog=Northwind;integrated security=True"
 SET repository.path="https://github.com/espiderinc/tutorial-demo-java.git"
-SET version.file="..\..\..\..\pom.xml"
-SET version.xpath="/project/version"
+SET version.file="pom.xml"
+SET version.xpath="//project/version"
 
 "D:\workspace\screencast\download\RoundhousE\console\rh.exe" /vf=%version.file% /vx=%version.xpath% /c=%connstring% /r=%repository.path% /env=LOCAL /ni /simple
