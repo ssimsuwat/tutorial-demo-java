31aa06dc34266ea01b7c524dc7cf1c394d1830a4
ssimsuwat <ssimsuwat@hotmail.com>
2017-05-01 00:25:25 +0700

    change the connstring property

--- src/main/resources/db/buildDB.bat
+++ src/main/resources/db/buildDB.bat
@@ -1,8 +1,8 @@
 @echo off
 SET DIR=%~d0%~p0%
 
-SET connstring="data source=TUTORIAL\TUTORIAL;initial catalog=Northwind;integrated security=True"
-SET repository.path="https://github.com/espiderinc/tutorial-demo-java.git"
+SET connstring="data source=TUTORIAL\TUTORIAL;initial catalog=Northwind;user=sa;password=Yukon900"
+SET repository.path="https://github.com/ssimsuwat/tutorial-demo-java.git"
 SET version.file="..\..\..\..\pom.xml"
 SET version.xpath="//project/version"
 
